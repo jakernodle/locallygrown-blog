42b7a78f5bf6528959cb03cdbb3383c9229dc282
JohnAnge Kernodle <jakernodle@gmail.com>
2023-01-11 21:29:54 -0500

    logo fix, remove rss

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,15 @@ import { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
+import logo from '../public/Locallygrowntextlabel.png'
+import Image from 'next/image'
 
 const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: false },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
@@ -68,37 +70,17 @@ const Header = ({ navBarTitle, fullWidth }) => {
       >
         <div className="flex items-center">
           <Link href="https://locallygrown.app/">
-            <a aria-label={BLOG.title}>
-              <div className="h-6">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
-                  <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
-                  </defs>
-                </svg>
-              </div>
-            </a>
+            <div style={{
+              position: 'absolute',
+              width: '260px',
+              height: '70%',
+              marginRight: 'auto',
+              '@media (max-width: 720px)': {
+                  width: '0px',
+              },
+            }}>
+                <Image src={logo} layout='fill' objectFit='contain' alt="local farms logo" />
+            </div>
           </Link>
           {navBarTitle
             ? (
